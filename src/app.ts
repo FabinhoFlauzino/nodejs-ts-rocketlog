dd29260d659904bb078d11d0da64630662e198b2
FabinhoFlauzino <fabinhoflauzino.dev@gmail.com>
2026-07-01 23:33:16 -0300

    Acrescentando códigos

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { errorHandling } from "./middlewares/error-handling";
-import { routes } from "./routes/inde";
+import { routes } from "./routes";
 
 const app = express();
 
