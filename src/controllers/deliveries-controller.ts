fcb36e26e03e501f32f042bc3e7ffff98874e021
FabinhoFlauzino <fabinhoflauzino.dev@gmail.com>
2026-07-02 23:14:06 -0300

    Efetuando cadastro de entrega e listagem

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,39 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { string, z } from "zod";
 
 class DeliveriesController {
   async create(request: Request, response: Response) {
-    return response.json({ message: "Ok" });
+    const bodySchema = z.object({
+      user_id: string().uuid(),
+      description: z.string(),
+    });
+
+    const { user_id, description } = bodySchema.parse(request.body);
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json();
+  }
+
+  async index(request: Request, response: Response) {
+    const deliveries = await prisma.delivery.findMany({
+      include: {
+        user: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    return response.json(deliveries);
   }
 }
 
